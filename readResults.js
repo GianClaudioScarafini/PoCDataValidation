d302f124d7f65217234787d92d141ac8f3a9b0a5
Gian Claudio Scarafini <g.claudio.scarafini@gmail.com>
2026-01-25 12:05:59 +0000

    add readResult console.log and config.json

---
+++ readResults.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// Read the JSON file
+const data = fs.readFileSync('./sample.json', 'utf8');
+const jsonData = JSON.parse(data);
+
+// Get all results
+const results = jsonData.data.elementsByElementGroup.results;
+
+
+// results.forEach((result,index)=>[
+//     console.log(`Parameter Name: ${result.name}`)
+// ])
+
+parameterList = new Array()
+
+// Print each result
+results.forEach((result, index) => {
+    console.log(`ID: ${result.id}`);
+    console.log(`Name: ${result.name}`);
+    console.log('Properties:');
+
+    result.properties.results.forEach(prop => {
+        console.log(`  - ${prop.name}: ${prop.value}`);
+        parameterList.push({
+            // name: result.name,
+            property : prop.name });
+    });
+});
+
+console.log("Parameter List:", parameterList);
